10a6fa2d1a6e6e99cb4e500ef1e496a8b313c208
hola <christianhbal@gmail.com>
2024-07-31 15:08:35 -0300

    Actualizacion 2

---
+++ bbdd.js
@@ -0,0 +1,272 @@
+export const USERS_BBDD= [
+    {
+      "_id": "66aa4358b228cfe19b2e5d99",
+      "index": 0,
+      "guid": "72e258d6-86e9-4c69-92ca-1ff8e9aa0ae7",
+      "isActive": false,
+      "balance": "$2,641.00",
+      "picture": "http://placehold.it/32x32",
+      "age": 34,
+      "eyeColor": "blue",
+      "name": "Cox Hodges",
+      "gender": "male",
+      "company": "ETERNIS",
+      "email": "coxhodges@eternis.com",
+      "phone": "+1 (946) 432-3014",
+      "address": "447 Sapphire Street, Osage, Nebraska, 7815",
+      "about": "Voluptate veniam incididunt consequat aute duis dolore quis irure do ad ex tempor. Sint culpa dolore anim dolore voluptate laboris commodo cillum cillum ad enim anim pariatur eu. Fugiat laborum nostrud consequat adipisicing fugiat elit fugiat. Id nostrud elit ut aliqua fugiat irure incididunt proident. Voluptate pariatur tempor commodo dolore eu magna minim velit quis. Eu enim deserunt officia in consequat fugiat nostrud aliquip. Lorem eiusmod occaecat dolore ea exercitation qui dolor exercitation.\r\n",
+      "registered": "2017-01-11T05:14:57 +03:00",
+      "latitude": 40.627581,
+      "longitude": 143.419265,
+      "tags": [
+        "nulla",
+        "mollit",
+        "elit",
+        "elit",
+        "sit",
+        "culpa",
+        "esse"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Bowen Morin"
+        },
+        {
+          "id": 1,
+          "name": "Morgan Romero"
+        },
+        {
+          "id": 2,
+          "name": "Calhoun Edwards"
+        }
+      ],
+      "greeting": "Hello, Cox Hodges! You have 7 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "66aa43581c2c8283eea2b52b",
+      "index": 1,
+      "guid": "e44d33b8-401e-49eb-9b61-39749bca64dc",
+      "isActive": true,
+      "balance": "$3,195.86",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Dona Hinton",
+      "gender": "female",
+      "company": "FARMEX",
+      "email": "donahinton@farmex.com",
+      "phone": "+1 (817) 512-2412",
+      "address": "123 Schenck Avenue, Chicopee, Virgin Islands, 3393",
+      "about": "Sunt et est dolor ipsum laboris et officia dolor cupidatat ad velit aliqua ullamco. Ullamco duis reprehenderit exercitation eu anim id ullamco ipsum occaecat anim voluptate velit. Velit id tempor mollit aliquip tempor consectetur excepteur sunt officia. Ex fugiat aliqua consequat consectetur id ullamco non consequat magna aute mollit excepteur. Et sit eu elit quis qui voluptate proident quis pariatur elit eu.\r\n",
+      "registered": "2018-10-16T05:22:28 +03:00",
+      "latitude": -87.57318,
+      "longitude": -139.302976,
+      "tags": [
+        "ipsum",
+        "eiusmod",
+        "exercitation",
+        "ipsum",
+        "anim",
+        "commodo",
+        "id"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Maura Delgado"
+        },
+        {
+          "id": 1,
+          "name": "Beatriz Jarvis"
+        },
+        {
+          "id": 2,
+          "name": "Gill Thomas"
+        }
+      ],
+      "greeting": "Hello, Dona Hinton! You have 1 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "66aa4358115884d1b63d5f94",
+      "index": 2,
+      "guid": "6d16bb26-f53a-49f0-ac3d-9e0910bb644a",
+      "isActive": false,
+      "balance": "$1,240.59",
+      "picture": "http://placehold.it/32x32",
+      "age": 30,
+      "eyeColor": "brown",
+      "name": "Hartman Kent",
+      "gender": "male",
+      "company": "TELPOD",
+      "email": "hartmankent@telpod.com",
+      "phone": "+1 (991) 452-3464",
+      "address": "585 Kingsland Avenue, Drummond, North Dakota, 4513",
+      "about": "Ad quis deserunt eiusmod proident voluptate culpa qui anim in nostrud est labore consequat ullamco. Non reprehenderit in non in fugiat velit proident labore eu nostrud consectetur in enim. Ea anim id consectetur proident deserunt dolor quis est sit.\r\n",
+      "registered": "2018-12-10T01:11:58 +03:00",
+      "latitude": 28.816929,
+      "longitude": -69.429236,
+      "tags": [
+        "tempor",
+        "sint",
+        "enim",
+        "minim",
+        "in",
+        "dolor",
+        "quis"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Gaines Velasquez"
+        },
+        {
+          "id": 1,
+          "name": "Mccray Baird"
+        },
+        {
+          "id": 2,
+          "name": "Laverne Wilson"
+        }
+      ],
+      "greeting": "Hello, Hartman Kent! You have 2 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "66aa435800330484f22529a2",
+      "index": 3,
+      "guid": "d62d1336-47a6-4b7f-a4c3-b2c8abf1ecf1",
+      "isActive": true,
+      "balance": "$2,325.44",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Jeannette Newton",
+      "gender": "female",
+      "company": "RODEOCEAN",
+      "email": "jeannettenewton@rodeocean.com",
+      "phone": "+1 (868) 477-3082",
+      "address": "442 Jodie Court, Bourg, Illinois, 9760",
+      "about": "Cupidatat cupidatat minim laboris ex deserunt proident reprehenderit culpa eiusmod dolore reprehenderit nisi. Id elit nulla sunt magna magna eiusmod laboris laborum velit aliquip sunt. Irure adipisicing pariatur fugiat officia exercitation irure quis do aliquip sunt duis sit pariatur. Proident non ipsum consequat do adipisicing aliqua exercitation.\r\n",
+      "registered": "2020-12-16T09:40:43 +03:00",
+      "latitude": -79.537502,
+      "longitude": 153.410267,
+      "tags": [
+        "incididunt",
+        "velit",
+        "labore",
+        "pariatur",
+        "voluptate",
+        "id",
+        "nulla"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "May Lowe"
+        },
+        {
+          "id": 1,
+          "name": "Melissa Christensen"
+        },
+        {
+          "id": 2,
+          "name": "Madeleine Poole"
+        }
+      ],
+      "greeting": "Hello, Jeannette Newton! You have 5 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "66aa4358103d2fdcf311fd53",
+      "index": 4,
+      "guid": "5bcc750c-a1ac-4b2e-b15d-40f94dcc5113",
+      "isActive": true,
+      "balance": "$1,018.70",
+      "picture": "http://placehold.it/32x32",
+      "age": 36,
+      "eyeColor": "blue",
+      "name": "Louella Horton",
+      "gender": "female",
+      "company": "HYPLEX",
+      "email": "louellahorton@hyplex.com",
+      "phone": "+1 (970) 574-2305",
+      "address": "704 Jackson Place, Bascom, District Of Columbia, 5240",
+      "about": "Do enim laboris occaecat eiusmod minim amet. Dolore esse sint aliquip deserunt minim. Non ullamco pariatur sunt fugiat dolore adipisicing. Qui adipisicing est exercitation occaecat elit cupidatat aliqua proident.\r\n",
+      "registered": "2015-12-29T03:17:15 +03:00",
+      "latitude": 24.837628,
+      "longitude": 85.874942,
+      "tags": [
+        "Lorem",
+        "enim",
+        "qui",
+        "irure",
+        "irure",
+        "laborum",
+        "anim"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Pearl Maxwell"
+        },
+        {
+          "id": 1,
+          "name": "Marla Simmons"
+        },
+        {
+          "id": 2,
+          "name": "Abby Hutchinson"
+        }
+      ],
+      "greeting": "Hello, Louella Horton! You have 9 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "66aa43584c3a444c167c6e81",
+      "index": 5,
+      "guid": "e7d950e0-7f3c-44a8-b69f-7fc0399617bc",
+      "isActive": false,
+      "balance": "$3,103.56",
+      "picture": "http://placehold.it/32x32",
+      "age": 30,
+      "eyeColor": "green",
+      "name": "Gomez Gonzales",
+      "gender": "male",
+      "company": "VALPREAL",
+      "email": "gomezgonzales@valpreal.com",
+      "phone": "+1 (839) 504-2869",
+      "address": "899 Box Street, Kimmell, Montana, 5253",
+      "about": "Non ex ad do enim non ipsum sint elit esse adipisicing dolore veniam occaecat in. Duis laboris ullamco aliquip mollit eiusmod quis sit irure cillum pariatur ullamco proident laborum quis. Ad ipsum sunt labore irure. Officia ad fugiat deserunt nisi quis cillum do officia aliqua sunt culpa minim. Ea voluptate sunt nostrud cillum irure. Elit incididunt reprehenderit laborum ipsum id.\r\n",
+      "registered": "2014-03-14T07:00:21 +03:00",
+      "latitude": 47.269958,
+      "longitude": -169.503734,
+      "tags": [
+        "est",
+        "sint",
+        "sit",
+        "qui",
+        "enim",
+        "non",
+        "eu"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Reeves Peters"
+        },
+        {
+          "id": 1,
+          "name": "Henrietta Smith"
+        },
+        {
+          "id": 2,
+          "name": "Cruz Benson"
+        }
+      ],
+      "greeting": "Hello, Gomez Gonzales! You have 4 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]
--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 console.clear();
 import express from "express";
+import dotenv from "dotenv";
+import {USERS_BBDD} from "./bbdd.js";
 
-const PORT=3000;
+dotenv.config();
+
+const PORT=2000;
 const expressApp= express();
 
 expressApp.use(express.json());
 expressApp.use(express.text());
+
+expressApp.get("/account/:guid", (req,res) => {
+    const user= USERS_BBDD.find(user => user.guid === req.params.guid);
+
+    if(!user) res.status(404).send();
+
+    res.send(user);
+});
+
+expressApp.post("/account", (req,res) => {
+
+});
+
+expressApp.patch("/account", (req,res) => {
+
+});
+
+expressApp.delete("/account", (req,res) => {
+
+});
+
 
 expressApp.get("/cuenta/:idcuenta/:idinventado", (req,res) => {
     console.log(req.params);
